2707339a13a4c59ac363e06d96f8f1a2a1f5fd5f
Bart Thate <bthate@dds.nl>
2019-12-02 15:55:17 +0100

    update mods

--- obdev/mods.py
+++ obdev/mods.py
@@ -1,6 +1,36 @@
 from ob import k
 from ob.shl import set_completer
 
+def get_mods(h, ms):
+    """ walk packages and load modules into the handler. """
+    from ob import k
+    modules = []
+    for mn in ms.split(","):
+        if not mn:
+            continue
+        m = None
+        try:
+            m = h.walk("obot.%s" % mn)
+        except ModuleNotFoundError as ex:
+            if mn not in str(ex):
+                logging.error(get_exception())
+            try:
+                    m = h.walk("%s.%s" % (h.cfg.name, mn))
+            except ModuleNotFoundError:
+                try:
+                    m = h.walk("ob.%s" % mn)
+                except ModuleNotFoundError as ex:
+                    if mn not in str(ex):
+                        logging.error(get_exception())
+                    try:
+                        m = h.walk(mn)
+                    except ModuleNotFoundError as ex:
+                        if mn not in str(ex):
+                        logging.error(get_exception())
+        if m:
+            modules.extend(m)
+    return modules
+
 def load(event):
     if not event.args:
         event.reply("|".join({modules[x].split(".")[-1] for x in k.modules}))
@@ -8,7 +38,7 @@ def load(event):
     m = []
     for name in event.args[0].split(","):
         name = event.args[0]
-        m.extend(mods(k, name))
+        m.extend(get_mods(k, name))
         k.init(name)
     set_completer(k.cmds)
     if m:
